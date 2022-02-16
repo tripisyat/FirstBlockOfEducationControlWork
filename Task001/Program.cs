a7ba95ab10f86cb61b4146c0ba66e4561be3b881
Dmitry Lopatinsky <Lopatinskyda@gmail.com>
2022-02-16 23:35:50 +0400

    Method for counting added

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] stringArray = new string[] {"abcde", "abc", "ab","abcd","abc"};
+
+int CountForNewArray(string[] ArrayForCounting)
+{
+    int count = 0;
+    for (int i = 0; i < ArrayForCounting.Length; i++)
+    {
+        if (ArrayForCounting[i].Length <=3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
